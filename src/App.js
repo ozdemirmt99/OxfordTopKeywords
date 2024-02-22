28c51a53e6e1fd26e96ebdfddce9937fd9180c2e
ozdemirmt99 <ozdermirmt99@gmail.com>
2024-02-22 18:42:18 +0300

    applyed some changes for performance

--- src/App.js
+++ src/App.js
@@ -7,30 +7,33 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  useEffect(() => {
+  const [allKeywords, setAllKeywords] = useState({});
 
+  useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex(currentIndex + 1)
+      setCurrentIndex((prevIndex) => prevIndex + 1);
     }, 350);
 
     return () => clearInterval(interval);
-  }, [currentIndex]);
-  
-  
-  let sizeTwoThousand = Object.keys(twoThousand);
-  let sizeThreeThousand = Object.keys(threeThousand);
-  let allKeywords = {};
-  let index = 0;
+  }, []);
 
-  sizeTwoThousand.map((e) => {
-    allKeywords[index] = twoThousand[e];
-    index++;
-  });
-  sizeThreeThousand.map((e) => {
-    allKeywords[index] = threeThousand[e];
-    index++;
-  });
-  
+  useEffect(() => {
+    let keywords = {};
+    let index = 0;
+
+    Object.keys(twoThousand).forEach((key) => {
+      keywords[index] = twoThousand[key];
+      index++;
+    });
+
+    Object.keys(threeThousand).forEach((key) => {
+      keywords[index] = threeThousand[key];
+      index++;
+    });
+
+    setAllKeywords(keywords);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
--- src/Keyword.jsx
+++ src/Keyword.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 export default function Keyword(props) {
   let current = props.dict;
-
-  let keys = Object.keys(current);
+  let keys;
+  if (current) {
+    keys = Object.keys(current);
+  }
 
   return (
     <>
-      {keys.map((index,e) => {
+      {keys?.map((index,e) => {
         if(index<3)
         return (<h1 style={{color:"yellow"}} key={index}>{current[e]}</h1>);
         else
