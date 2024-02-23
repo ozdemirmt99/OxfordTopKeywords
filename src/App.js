3fc67482eac959c7c72a2711be973fa543c6ba9f
ozdemirmt99 <ozdermirmt99@gmail.com>
2024-02-23 04:48:31 +0300

    start stop mechanism

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import threeThousand from "./Keywords/threeThousand.json";
 import twoThousand from "./Keywords/twoThousand.json";
 import Keyword from "./Keyword";
 import { useEffect, useState } from "react";
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [allKeywords, setAllKeywords] = useState({});
+  const [continued, setContinued] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => prevIndex + 1);
+      if (continued) {
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }
     }, 350);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [continued]);
+
+  const pause = (status) => {
+    setContinued(status);
+  };
 
   useEffect(() => {
     let keywords = {};
@@ -37,8 +46,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div style={{ width: "100vw" }}>
+        <div>
+          <Button  variant="primary" onClick={() => pause(!continued)}>
+            {continued ? "Duraklat" : "Başlat"}
+          </Button>
+        </div>
+        <div style={{ width: "100vw", height: "65vh" }}>
           <Keyword dict={allKeywords[currentIndex]} key={currentIndex} />
         </div>
       </header>
